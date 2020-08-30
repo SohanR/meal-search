024340ec272ef4e41469a6bb5bfba9555f4cc2db
SohanR <mizan.rahman66d@gmail.com>
2020-08-30 20:52:52 +0600

    missing =

--- src/js/index.js
+++ src/js/index.js
@@ -78,7 +78,7 @@ function addMealToDOM(meal) {
     <div class="single-meal">
       <h1>${meal.strMeal}</h1>
       <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-      <div class-"single-meal-info">
+      <div class="single-meal-info">
         ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
         ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
       </div>
